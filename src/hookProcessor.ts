5fb0e6d8391674678cc14b9247dba4ea09d60b72
Hoang Pham <huyhoang8a5@gmail.com>
2019-04-27 10:16:24 +0800

    Small code cleanup

--- src/hookProcessor.ts
+++ src/hookProcessor.ts
@@ -11,37 +11,33 @@ export const processHook = async (hook : Root) => {
     for (const entry of hook.entry) {
         for (const change of entry.changes) {
             const {item} = change.value
-            if(item === 'comment') {
+            if (item === 'comment') {
                 await processPostComment(change.value)
             }
         }
     }
 }
 
-const shouldReply = (post: PostInfo) : boolean => {
-    const tags = ['#tuvi-autobot', '#girl-autobot']
-    return tags.some(tag => post.message.includes(tag))
-}
-
 const processPostComment = async (changeValue : Value) => {
     const { comment_id: commentId, from: {name}, message, parent_id, post_id } = changeValue
-    if (commentId) {
-        const comment : Comment = { commentId, name, message }
+    if (parent_id !== post_id) {
+        console.log(`Comment ${commentId} can not be replied to. Ignore it!`)
+        return
+    }
 
-        if (parent_id !== post_id) {
-            console.log(`Comment ${commentId} can not be replied to. Ignore it!`)
+    if (!commentId) return
+    
+    const post = await getPostInfo(post_id)
+    for (const processor of processors) {
+        const tag = processor.tag
+        if (processor.shouldReply(post)) {
+            console.log(`Tag found: ${tag}`)
+
+            const comment : Comment = { commentId, name, message }
+            const reply = processor.getReply(comment)
+            await replyToComment(commentId, reply)
         } else {
-            const post = await getPostInfo(post_id)
-            for (const processor of processors) {
-                const tag = processor.tag
-                if (processor.shouldReply(post)) {
-                    console.log(`Tag found: ${tag}`)
-                    const reply = processor.getReply(comment)
-                    await replyToComment(commentId, reply)
-                } else {
-                    console.log(`Tag ${tag} not found in post`)
-                }
-            }
+            console.log(`Tag ${tag} not found in post`)
         }
     }
 }
